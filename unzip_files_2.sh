797cd4b72111fcca500f5e2475654aeb5fc46d8b
Tim <tbarry2@andrew.cmu.edu>
2023-11-15 19:28:34 -0500

    fix fp in unzip files

--- unzip_files_2.sh
+++ unzip_files_2.sh
@@ -1,6 +1,7 @@
 source ~/.research_config
 echo $LOCAL_REPLOGLE_2022_DATA_DIR
 rd7_dir=$LOCAL_REPLOGLE_2022_DATA_DIR"raw/rd7/"
-tar -xvzf $rd7_dir"raw.mtx.tar.gz"
-gzip -dv $rd7_dir/"rpe1_other" *
+cd $rd7_dir
+tar -xvzf "raw.mtx.tar.gz"
+# gzip -dv "rpe1_other" *
 rm $rd7_dir"raw.mtx.tar.gz"
