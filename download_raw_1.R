4c3e189b822685311829ec197053ee1f05058f74
timothy-barry <tbarry2@andrew.cmu.edu>
2024-04-29 19:12:48 -0400

    update import script

--- download_raw_1.R
+++ download_raw_1.R
@@ -2,17 +2,17 @@ repl_offsite <- paste0(.get_config_path("LOCAL_REPLOGLE_2022_DATA_DIR"))
 
 # load R.utils; increase timeout to 5 hours
 library(R.utils)
-options(timeout = 5 * 60 * 60)
+options(timeout = 10 * 60 * 60)
 
 # create raw directory; also create subdirectories for the four datasets
 raw_data_dir <- paste0(repl_offsite, "raw/")
 raw_data_dir_rep <- paste0(raw_data_dir, c("kd8", "rd7", "kd6"))
 for (dir in raw_data_dir_rep) {
-  if (!dir.exists(dir)) dir.create(dir, recursive = TRUE) 
+  if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
 }
 
 # URL of data
-data_urls <- c(rd7 = "https://plus.figshare.com/ndownloader/files/36000888",
+data_urls <- c(#rd7 = "https://plus.figshare.com/ndownloader/files/36000888",
                #kd8_ultima = "https://plus.figshare.com/ndownloader/files/36000884",
                kd6 = "https://plus.figshare.com/ndownloader/files/36000843",
                kd8 = "https://plus.figshare.com/ndownloader/files/36000572")
